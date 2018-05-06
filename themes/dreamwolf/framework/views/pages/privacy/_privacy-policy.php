1fbacfe66762953316f72e4e7a51e1c99916f7cc
codingwithchris <chris@codewolfseven.com>
2018-05-06 09:30:19 -0400

    Update Provacy Policy Spacing

--- themes/dreamwolf/framework/views/pages/privacy/_privacy-policy.php
+++ themes/dreamwolf/framework/views/pages/privacy/_privacy-policy.php
@@ -1,6 +1,6 @@
-<div class="row _pts _pbs _bg-l-gray">
+<div class="row _ptl _pbl _bg-l-gray">
 	<div class="container -max -width">
-		<h2 class="_l-green">The Playground Theatre takes privacy very seriously.</h2>
+		<h2 class="_l-green">The Playground takes privacy very seriously.</h2>
 		<p class="_m-gray">
 			1. We will NEVER give or sell any information entered to any of our contact forms to any 3rd party.<br/>
 			2. This website uses cookies to track and record visitor analytics data.<br/>
