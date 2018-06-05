1556d9cf5aaeaca55cd5da933407cad4ce58d548
codingwithchris <chris@codewolfseven.com>
2018-06-05 18:50:31 -0400

    Audition Intro Udpates

--- themes/dreamwolf/framework/views/pages/audition/_audition-intro.php
+++ themes/dreamwolf/framework/views/pages/audition/_audition-intro.php
@@ -5,9 +5,9 @@
 		<div class="-fill">
 
 			<h2 class="_gray">Welcome to our<br/><span class="_l-green">Audition Request Page</span></h2>
-			<p class="_gray -larger _width-max-600">Take some time and look around because there is a lot of important info, including role breakdowns, audition dates, times, locations and more!</p>
+			<p class="_gray -larger _width-max-700">Take some time and look around because there is a lot of important info, including role breakdowns, audition dates, times, locations, and more!</p>
 
-			<p class="_gray _width-max-600">When you're ready, simply fill out the request form towards the bottom of this page.</p>
+			<p class="_gray -larger _width-max-700">When you're ready, simply fill out the request form towards the bottom of this page.</p>
 
 		</div>
 
